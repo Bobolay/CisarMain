58425f18726ec06cf042ae8061227b261d6aa273
pasha <p.korenev@voroninstudio.eu>
2016-09-21 18:44:11 +0300

    locales

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,7 +3,7 @@ class SitemapController < ApplicationController
   def index
     @content = Pages.sitemap_xml.try(:content)
     if @content.blank?
-      @entries = Cms::SitemapElement.entries
+      @entries = Cms::SitemapElement.entries[:uk]
     end
 
     render layout: false
