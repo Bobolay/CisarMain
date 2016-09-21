f346f4190a20d6974c1555dd94e165f724085131
pasha <p.korenev@voroninstudio.eu>
2016-09-21 13:21:19 +0300

    fix

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -76,7 +76,7 @@ RailsAdmin.config do |config|
 
   config.model  Cms::SitemapElement do
     field :display_on_sitemap
-    field :change_freq, :enum
+    field :changefreq
     field :priority
   end
 
